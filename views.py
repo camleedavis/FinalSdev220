3c3b9452554e99bcbe9f79aeb7017fb431f63738
NahumLa <112948696+NahumLa@users.noreply.github.com>
2022-12-18 21:14:21 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -8,4 +12,20 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+    
+def home(request):
+    return render(request, 'users/home.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Account made. Login!')    
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
