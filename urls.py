5078ea4190c8d8e80bbf1d0c8491f1b180ea1e7a
NahumLa <112948696+NahumLa@users.noreply.github.com>
2022-12-18 21:16:24 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
 ]
